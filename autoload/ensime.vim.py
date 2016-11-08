476ad418f60e68f58eeccb294e839aa2a08570f6
Pavel <paultolk@yahoo.com>
2016-11-07 23:19:19 -0500

    fixed check for file path ends

--- autoload/ensime.vim.py
+++ autoload/ensime.vim.py
@@ -9,10 +9,18 @@ import vim
 
 def ensime_init_path():
     path = os.path.abspath(inspect.getfile(inspect.currentframe()))
-    if path.endswith('/rplugin/python/ensime.py'):  # nvim rplugin
+    dir_file1 = os.path.split(path)
+    path_fn = dir_file1[1]
+    dir_file2 = os.path.split(dir_file1[0])
+    path_dir1 = dir_file2[1]
+    dir_file3 = os.path.split(dir_file2[0])
+    path_dir2 = dir_file3[1]
+    expected_nvim_path_end = ['rplugin', 'python', 'ensime.py']
+    expected_vim_path_end = ['autoload','ensime.vim.py']
+    if expected_nvim_path_end == [path_dir2, path_dir1, path_fn]: # nvim rplugin
         sys.path.append(os.path.join(
             os.path.dirname(os.path.dirname(os.path.dirname(path)))))
-    elif path.endswith('/autoload/ensime.vim.py'):  # vim plugin
+    elif expected_vim_path_end == [path_dir1, path_fn]: # vim plugin
         sys.path.append(os.path.join(
             os.path.dirname(os.path.dirname(path))))
 
